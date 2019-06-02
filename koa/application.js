5be44ba0f274a63243da861bbd1045408d71f9c8
小蜗 <627054877@qq.com>
2019-06-02 11:21:41 +0800

    0602

--- koa/application.js
+++ koa/application.js
@@ -11,13 +11,13 @@ let response = require('./response');
 class Koa {
   constructor() {
     // 私有属性
-    this.callbackFn;
     this.context = context;
     this.request = request;
     this.response = response;
+    this.middlewares = []; // 多个中间件
   }
   use(cb) {
-    this.callbackFn = cb; // 调用use方法的时候，将方法存到callbackFn中
+    this.middlewares.push(cb);
   }
   // 创建上下文
   createContext(req, res) {
@@ -28,17 +28,32 @@ class Koa {
     ctx.res = ctx.response.res = res; // 将原生的res也赋值到ctx上
     return ctx;
   }
+  // 组合
+  compose(ctx, middlewares) {
+    function dispatch(index) {
+      // 执行到最后一个中间件时，直接返回，执行完毕
+      if (index === middlewares.length) return Promise.resolve();
+      let middleware = middlewares[index];
+      // 递归创建嵌套的promise，每个promise必须等待内部promise执行完毕后，再执行
+      return Promise.resolve(middleware(ctx, () => dispatch(index + 1)));
+    }
+    return dispatch(0);
+  }
   // 处理请求
   handleRequest(req, res) {
     res.statusCode = 404; // 默认页面找不到
     let ctx = this.createContext(req, res);
-    this.callbackFn(ctx); // 当回调函数执行后，ctx.body值就会发生变化
-    let body = ctx.body;
-    if (typeof body === 'undefined') {
-      res.end('Not Found');
-    } else if (typeof body === 'string') {
-      res.end(body);
-    }
+    // 当回调函数执行后，ctx.body值就会发生变化
+    let composeMiddleware = this.compose(ctx, this.middlewares);
+    // 当此promise执行完成之后，再执行res.end()
+    composeMiddleware.then(() => {
+      let body = ctx.body;
+      if (typeof body === 'undefined') {
+        res.end('Not Found');
+      } else if (typeof body === 'string') {
+        res.end(body);
+      }
+    });
   }
   // 监听端口时，启动服务
   listen() {
